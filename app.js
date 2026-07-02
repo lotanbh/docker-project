66511ce734b823d0bd3d70014fbbe2afd469e9e6
lotan <lotanbh@gmail.com>
2026-07-02 19:42:46 +0300

    creating production

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const express = require('express'); 
 const connectDB = require("./config/db");
 const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const userRoutes = require("./routes/users");
 
@@ -13,6 +15,12 @@ const http = require('http');
 
 // מאזינים לפורט לוקאלי מסויים 
 const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://127.0.0.1:5500',
+    process.env.RENDER_EXTERNAL_URL
+].filter(Boolean); // מסנן ערכים ריקים
 
 //  מאתחל
 const app = express();
@@ -22,16 +30,44 @@ connectDB();
 
 // השרת שלנו יכול לפרסר מידע בצורת json
 app.use(express.json());
-app.use(cors()); 
+// app.use(cors()); // מאפשר גישה בcors לכל הURLים
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+             callback(null, true);
+            } else {
+             callback(new Error('Not allowed by CORS policies'));
+            }
+        }
+    })
+);
 
-// // מאפשר גישה בcors רק לURL שהגודר
+if (!process.env.JWT_SECRET) {
+    console.error('JWT_SECRET is not set in the environment variables.');
+    process.exit(1); // Exit the application with an error code
+}
+
+// // מאפשר גישה בcors רק לURL שה Gord
 // app.use(cors({
 //     origin: "http://127.0.0.1:5501"
 // }));
 
+app.get("/health", (req , res) => {
+    const dbStatus = ["connected", "disconnected", "connecting", "disconnecting"];
+    const dbConnected = mongoose.connection.readyState === 1;
+
+    res.status(dbConnected ? 200 : 503).json({
+         status: dbConnected ? "OK" : "Error",
+         db: dbStatus[mongoose.connection.readyState],
+         runtime: `${Math.floor(process.uptime())} seconds`,
+         environment: process.env.NODE_ENV || "development",
+         timestamp: new Date().toISOString()
+        });
+});
 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/api/users", userRoutes);
 
